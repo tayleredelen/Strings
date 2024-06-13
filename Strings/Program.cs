fe4f8a4f0764543086fc157a01a072b39781d7aa
tayleredelen <taylerledelen@gmail.com>
2024-06-13 16:45:40 -0500

    Strings chapter complete

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Tae";
+            var lastName = "Albert";
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+            Console.WriteLine(myFullName);
+
+            var names = new string[3] { "CT", "Gordon", "Tegan" };
+            var formattedNames = string.Join(", ", names); //the first value is the separater, the second is the array
+            Console.WriteLine("My favorite pets: " + formattedNames);
+
+        }
+    }
+}
 
